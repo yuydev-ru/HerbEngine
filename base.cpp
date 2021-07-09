4c3d99d468d3a50aa7f31d9e388e395d57204b17
Michael Chernigin <michaelchernigin@gmail.com>
2021-07-09 14:43:20 +0400

    Добавили механику однократного нажатия

--- base.cpp
+++ base.cpp
@@ -45,6 +45,7 @@ main()
     GameState state {};
     state.running = true;
     state.window = &window;
+    bool isPushed = false;
 
     Storage storage;
     initializeEngine(&state, &storage);
@@ -56,6 +57,7 @@ main()
 
         while (window.pollEvent(event))
         {
+
             if (event.type == sf::Event::Closed)
             {
                 window.close();
@@ -72,14 +74,32 @@ main()
             if (event.type == sf::Event::KeyPressed && state.axisData.count(event.key.code))
             {
                 KeyData pressedKeyData = state.axisData[event.key.code];
-                state.axes[pressedKeyData.axis] = pressedKeyData.value;
+                if (pressedKeyData.axisType == "push")
+                {
+                    if (isPushed)
+                    {
+                        pressedKeyData.value = 0;
+                    }
+                    else
+                    {
+                        isPushed = true;
+                        pressedKeyData.value = 1;
+                        std::cout << "E";
+                    }
+                }
+                else
+                {
+                    state.axes[pressedKeyData.axis] = pressedKeyData.value;
+                }
             }
 
             if (event.type == sf::Event::KeyReleased && state.axisData.count(event.key.code))
             {
                 sf::Keyboard::Key oppositeKey;
-                for (const auto& it : config.keys) {
-                    if (it.second == event.key.code) {
+                for (const auto& it : config.keys)
+                {
+                    if (it.second == event.key.code)
+                    {
                         oppositeKey =
                                 config.keys[config.oppositeKeys[it.first]];
                     }
@@ -87,6 +107,11 @@ main()
 
                 KeyData pressedKeyData = state.axisData[event.key.code];
                 KeyData oppositeKeyData = state.axisData[oppositeKey];
+
+                if (pressedKeyData.axisType == "push")
+                {
+                    isPushed = false;
+                }
 
                 state.axes[pressedKeyData.axis] =
                     (sf::Keyboard::isKeyPressed(oppositeKey)) ? oppositeKeyData.value : 0;
